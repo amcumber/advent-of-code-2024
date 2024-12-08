584125f403ce4c72571004d8458d75216b9af945
Aaron McUmber <20608451+amcumber@users.noreply.github.com>
2024-12-08 10:26:57 -0700

    feat(day06): solve part 1 - debugging part 2

--- aoc_2024/day06/soln.py
+++ aoc_2024/day06/soln.py
@@ -1,30 +1,35 @@
-import re
-from collections import defaultdict
 from dataclasses import dataclass
+from enum import Enum, auto
 from pathlib import Path
 
 import click
 
 from ..core import get_input_file, read_file
 
+
+class Face(Enum):
+    up = auto()
+    right = auto()
+    down = auto()
+    left = auto()
+    standing = auto()
+
+
 WALL = "#"
-AGENTS = ["^", ">", "v", "<"]
-WALK = {
-    "^": (-1, 0),
-    ">": (0, 1),
-    "v": (1, 0),
-    "<": (0, -1),
-    "": None,
+AGENT_FACES = ["^", ">", "v", "<"]
+AGENT2ENUM = {
+    "^": Face.up,
+    ">": Face.right,
+    "v": Face.down,
+    "<": Face.left,
 }
 TURN = {
-    "^": ">",
-    ">": "v",
-    "v": "<",
-    "<": "^",
-    "": None,
+    Face.up: Face.right,
+    Face.right: Face.down,
+    Face.down: Face.left,
+    Face.left: Face.up,
+    Face.standing: Face.standing,
 }
-_gbl_max_x = None
-_gbl_max_y = None
 
 
 def _get_targets(data: list[str], target: str) -> list[tuple[int, int]]:
@@ -42,15 +47,21 @@ def get_walls(data):
 
 
 def get_agent(data):
-    for agent_face in AGENTS:
-        if agent_list := _get_targets(data, agent_face):
-            return Agent(Point(*agent_list.pop()), agent_face)
+    for agent_face in AGENT_FACES:
+        face = AGENT2ENUM[agent_face]
+        if agent_pos := _get_targets(data, agent_face):
+            return Agent(Point(*agent_pos.pop()), face)
 
 
 @dataclass
 class Point:
     x: int
     y: int
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError("Must be same type")
+        return (self.x, self.y) == (other.x, other.y)
 
 
 @dataclass
@@ -63,7 +74,7 @@ class Agent:
     """agent position and facing direction"""
 
     pos: Point
-    face: str
+    face: Face
 
     @property
     def x(self):
@@ -73,89 +84,109 @@ class Agent:
     def y(self):
         return self.pos.y
 
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError("Must be same type")
+        if self.pos != other.pos:
+            return False
+        return self.face == other.face
 
-def get_walls_row(agent_row: int, walls: list[Wall]):
-    return [wall for wall in walls if wall.y == agent_row]
+
+def get_walls_row(agent: Agent, walls: list[Wall]):
+    return [wall for wall in walls if wall.x == agent.x]
 
 
-def get_walls_col(agent_col: int, walls: list[Wall]):
-    return [wall for wall in walls if wall.x == agent_col]
+def get_walls_col(agent: Agent, walls: list[Wall]):
+    return [wall for wall in walls if wall.y == agent.y]
 
 
 def walk_up(agent, walls) -> Agent:
     collided_wall = None
-    for wall in get_walls_col(agent.y, walls):
+    for wall in get_walls_col(agent, walls):
         is_wall_fits = wall.x < agent.x
-        is_wall_close = collided_wall is not None and wall.x > collided_wall.x
+        is_wall_close = True
+        if collided_wall is not None:
+            is_wall_close = wall.x > collided_wall.x
         if is_wall_fits and is_wall_close:
             collided_wall = wall
     if collided_wall is None:
-        return Agent(Point(_gbl_max_x, agent.y), "")
+        return Agent(Point(0, agent.y), Face.standing)
     return Agent(Point(collided_wall.x + 1, agent.y), TURN[agent.face])
 
 
-def walk_right(agent, walls):
+def walk_right(agent, walls, max_y=999):
     collided_wall = None
-    for wall in get_walls_row(agent.x, walls):
+    for wall in get_walls_row(agent, walls):
         is_wall_fits = wall.y > agent.y
-        is_wall_close = collided_wall is not None and wall.y < collided_wall.y
+        is_wall_close = True
+        if collided_wall is not None:
+            is_wall_close = wall.y < collided_wall.y
         if is_wall_fits and is_wall_close:
             collided_wall = wall
     if collided_wall is None:
-        return Agent(Point(agent.x, _gbl_max_y), "")
+        return Agent(Point(agent.x, max_y), Face.standing)
     return Agent(Point(agent.x, collided_wall.y - 1), TURN[agent.face])
 
 
-def walk_down(agent, walls):
+def walk_down(agent, walls, max_x=999):
     collided_wall = None
-    for wall in get_walls_col(agent.y, walls):
+    for wall in get_walls_col(agent, walls):
         is_wall_fits = wall.x > agent.x
-        is_wall_close = collided_wall is not None and wall.x < collided_wall.x
+        is_wall_close = True
+        if collided_wall is not None:
+            is_wall_close = wall.x < collided_wall.x
         if is_wall_fits and is_wall_close:
             collided_wall = wall
     if collided_wall is None:
-        return Agent(Point(_gbl_max_x, agent.y), "")
+        return Agent(Point(max_x, agent.y), Face.standing)
     return Agent(Point(collided_wall.x - 1, agent.y), TURN[agent.face])
 
 
 def walk_left(agent, walls):
     collided_wall = None
-    for wall in get_walls_row(agent.x, walls):
+    for wall in get_walls_row(agent, walls):
         is_wall_fits = wall.y < agent.y
-        is_wall_close = collided_wall is not None and wall.y > collided_wall.y
+        is_wall_close = True
+        if collided_wall is not None:
+            is_wall_close = wall.y > collided_wall.y
         if is_wall_fits and is_wall_close:
             collided_wall = wall
     if collided_wall is None:
-        return Agent(Point(agent.x, _gbl_max_y), "")
+        return Agent(Point(agent.x, 0), Face.standing)
     return Agent(Point(agent.x, collided_wall.y + 1), TURN[agent.face])
 
 
-def step(agent_pos: list[Agent], walls: list[Wall]) -> bool:
+def step(agent_pos: list[Agent], walls: list[Wall], maxes=tuple[int, int]) -> bool:
     agent = agent_pos[-1]
+    max_x, max_y = maxes
     match agent.face:
-        case "^":
+        case Face.up:
             new_agent = walk_up(agent, walls)
-        case ">":
-            new_agent = walk_right(agent, walls)
-        case "v":
-            new_agent = walk_down(agent, walls)
-        case "<":
+        case Face.right:
+            new_agent = walk_right(agent, walls, max_y=max_y)
+        case Face.down:
+            new_agent = walk_down(agent, walls, max_x=max_x)
+        case Face.left:
             new_agent = walk_left(agent, walls)
-        case "":
+        case Face.standing:
             return True
+    if new_agent in agent_pos:
+        return True
+    if new_agent.pos == agent_pos[-1].pos:
+        agent_pos[-1] = new_agent
     agent_pos.append(new_agent)
     return False
 
 
-def walk_agent(agent: tuple[int, int], walls: list[tuple[int, int]]):
+def walk_agent(agent: Agent, walls: list[Wall], maxes=tuple[int, int]) -> list[Agent]:
     agent_pos = [agent]
     stop = False
     while not stop:
-        stop = step(agent_pos, walls)
+        stop = step(agent_pos, walls, maxes=maxes)
     return agent_pos
 
 
-def infill_agents(first, second):
+def infill_agents(first: Agent, second: Agent):
     if first.x == second.x:
         axis = 1
         diff = second.y - first.y
@@ -166,7 +197,7 @@ def infill_agents(first, second):
         diff = second.x - first.x
         start = first.x
         stop = second.x
-    direction = int(diff > 0)
+    direction = 2 * int(diff > 0) - 1
     for ele in range(start, stop, direction):
         if axis:
             yield Agent(Point(first.x, ele), second.face)
@@ -174,32 +205,75 @@ def infill_agents(first, second):
             yield Agent(Point(ele, first.y), second.face)
 
 
-def calculate_steps(agent_pos):
+def calculate_steps(agent_pos: list[Agent]) -> int:
     first_agent = agent_pos.pop(0)
     infilled = []
     for next_agent in agent_pos:
         infilled.extend(list(infill_agents(first_agent, next_agent)))
+        first_agent = next_agent
     return len(set((agent.x, agent.y) for agent in infilled))
 
 
-def set_glb(data):
-    global _gbl_max_x
-    global _gbl_max_y
-    _gbl_max_x = len(data)
-    _gbl_max_y = len(data[0])
+def get_dims(data):
+    return len(data), len(data[0])
 
 
 def main_part1(data):
     """Solution to day 05 part 1"""
     agent = get_agent(data)
     walls = get_walls(data)
-    set_glb(data)
-    agent_pos = walk_agent(agent, walls)
+    maxes = get_dims(data)
+    agent_pos = walk_agent(agent, walls, maxes=maxes)
     return calculate_steps(agent_pos)
+
+
+def _add_nearby(agent: Agent, maxes: tuple[int, int]) -> list[Wall]:
+    max_x, max_y = maxes
+    left = agent.pos.x - 1, agent.pos.y
+    right = agent.pos.x + 1, agent.pos.y
+    up = agent.pos.x, agent.pos.y - 1
+    down = agent.pos.x, agent.pos.y + 1
+    return [Wall(x, y) for x, y in (left, right, up, down) if x <= max_x and y <= max_y]
+
+
+def _get_naive_walls(agent_pos: list[Agent], maxes: tuple[int, int]) -> list[Wall]:
+    naive_walls = []
+    for agent in agent_pos:
+        naive_walls.extend(_add_nearby(agent, maxes))
+    wall_tups = set((wall.x, wall.y) for wall in naive_walls)
+
+    return [Wall(x, y) for x, y in wall_tups]
+
+
+def get_wall_candidates(
+    agent_pos: list[Agent], walls: list[Wall], maxes: tuple[int, int]
+) -> list[Wall]:
+    naive_cand = _get_naive_walls(agent_pos, maxes)
+    return [cand for cand in naive_cand if cand not in walls]
+
+
+def find_loops(
+    agent: Agent, walls: list[Wall], candidates: list[Wall], maxes: tuple[int, int]
+) -> list[Wall]:
+    successful = []
+    for cand in candidates:
+        tmp_walls = walls.copy()
+        tmp_walls.append(cand)
+        agent_pos = walk_agent(agent, tmp_walls, maxes=maxes)
+        if agent_pos[-1].face != Face.standing:
+            successful.append(cand)
+    return successful
 
 
 def main_part2(data):
     """Solution to day 05 part 2"""
+    agent = get_agent(data)
+    walls = get_walls(data)
+    maxes = get_dims(data)
+    agent_pos = walk_agent(agent, walls, maxes=maxes)
+    candidates = get_wall_candidates(agent_pos, walls, maxes)
+    loops = find_loops(agent, walls, candidates, maxes)
+    return len(loops)
 
 
 @click.group
--- aoc_2024/day06/test_day06.py
+++ aoc_2024/day06/test_day06.py
@@ -27,8 +27,102 @@ def ex_raw_input_p1(input_):
 
 @pytest.fixture
 def ex_raw_input_p2(input_):
-    expected = 123
+    expected = 6
     return input_, expected
+
+
+@pytest.mark.parametrize(
+    ["agent", "walls", "expected"],
+    [
+        ((0, 0), [(0, 5), (0, 1), (1, 5)], [(0, 5), (0, 1)]),
+        ((1, 0), [(0, 5), (0, 1), (1, 5)], [(1, 5)]),
+    ],
+)
+def test_get_walls_row(agent, walls, expected):
+    agent = soln.Agent(soln.Point(*agent), ">")
+    walls = [soln.Point(*wall) for wall in walls]
+    expected = [soln.Point(*wall) for wall in expected]
+    results = soln.get_walls_row(agent, walls)
+    assert len(results) == len(expected)
+    for r, e in zip(results, expected):
+        assert (r.x, r.y) == (e.x, e.y)
+
+
+@pytest.mark.parametrize(
+    ["agent", "walls", "expected"],
+    [
+        ((0, 0), [(5, 0), (1, 0), (5, 1)], [(5, 0), (1, 0)]),
+        ((0, 1), [(5, 0), (1, 0), (5, 1)], [(5, 1)]),
+    ],
+)
+def test_get_walls_col(agent, walls, expected):
+    agent = soln.Agent(soln.Point(*agent), soln.Face.right)
+    walls = [soln.Point(*wall) for wall in walls]
+    expected = [soln.Point(*wall) for wall in expected]
+    results = soln.get_walls_col(agent, walls)
+    assert len(results) == len(expected)
+    for r, e in zip(results, expected):
+        assert (r.x, r.y) == (e.x, e.y)
+
+
+@pytest.mark.parametrize(
+    ["agent", "walls", "expected"],
+    [
+        ((0, 0), [(0, 5)], (0, 4)),
+        ((0, 2), [(0, 1)], (0, 10)),
+        ((0, 2), [(0, 8), (0, 5)], (0, 4)),
+    ],
+)
+def test_walk_right(agent, walls, expected):
+    agent = soln.Agent(soln.Point(*agent), soln.Face.right)
+    walls = [soln.Point(*wall) for wall in walls]
+    results = soln.walk_right(agent, walls, max_y=10)
+    assert (results.x, results.y) == expected
+
+
+@pytest.mark.parametrize(
+    ["agent", "walls", "expected"],
+    [
+        ((0, 5), [(0, 0)], (0, 1)),
+        ((0, 5), [(0, 6)], (0, 0)),
+        ((0, 7), [(0, 2), (0, 5)], (0, 6)),
+    ],
+)
+def test_walk_left(agent, walls, expected):
+    agent = soln.Agent(soln.Point(*agent), soln.Face.left)
+    walls = [soln.Point(*wall) for wall in walls]
+    results = soln.walk_left(agent, walls)
+    assert (results.x, results.y) == expected
+
+
+@pytest.mark.parametrize(
+    ["agent", "walls", "expected"],
+    [
+        ((5, 0), [(0, 0)], (1, 0)),
+        ((5, 0), [(6, 0)], (0, 0)),
+        ((7, 0), [(2, 0), (5, 0)], (6, 0)),
+    ],
+)
+def test_walk_up(agent, walls, expected):
+    agent = soln.Agent(soln.Point(*agent), soln.Face.up)
+    walls = [soln.Point(*wall) for wall in walls]
+    results = soln.walk_up(agent, walls)
+    assert (results.x, results.y) == expected
+
+
+@pytest.mark.parametrize(
+    ["agent", "walls", "expected"],
+    [
+        ((0, 0), [(5, 0)], (4, 0)),
+        ((2, 0), [(1, 0)], (10, 0)),
+        ((2, 0), [(8, 0), (5, 0)], (4, 0)),
+    ],
+)
+def test_walk_down(agent, walls, expected):
+    agent = soln.Agent(soln.Point(*agent), soln.Face.down)
+    walls = [soln.Point(*wall) for wall in walls]
+    results = soln.walk_down(agent, walls, max_x=10)
+    assert (results.x, results.y) == expected
 
 
 def test_main_part1(ex_raw_input_p1):
@@ -41,3 +135,38 @@ def test_main_part2(ex_raw_input_p2):
     input_, expected = ex_raw_input_p2
     result = soln.main_part2(input_)
     assert result == expected
+
+
+@pytest.fixture
+def expected_cands():
+    return (
+        soln.Wall(x, y)
+        for x, y in [
+            (6, 3),
+            (7, 6),
+            (7, 7),
+            (8, 1),
+            (8, 3),
+            (9, 7),
+        ]
+    )
+
+
+def test_get_naive_walls(input_, expected_cands):
+    agent = soln.get_agent(input_)
+    walls = soln.get_walls(input_)
+    maxes = soln.get_dims(input_)
+    agent_pos = soln.walk_agent(agent, walls, maxes=maxes)
+    results = soln._get_naive_walls(agent_pos, maxes)
+    for e in expected_cands:
+        assert e in results
+
+
+def test_get_wall_candidates(input_, expected_cands):
+    agent = soln.get_agent(input_)
+    walls = soln.get_walls(input_)
+    maxes = soln.get_dims(input_)
+    agent_pos = soln.walk_agent(agent, walls, maxes=maxes)
+    results = soln.get_wall_candidates(agent_pos, walls, maxes)
+    for e in expected_cands:
+        assert e in results
