b23b3dec395e59b9ab069d5a58851a83aeda3a07
Aaron <20608451+amcumber@users.noreply.github.com>
2024-12-13 10:34:01 -0800

    debugging day9

--- aoc_2024/day09/soln.py
+++ aoc_2024/day09/soln.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, namedtuple
+from dataclasses import dataclass
 from itertools import product, zip_longest
 from pathlib import Path
 
@@ -6,22 +7,22 @@ import click
 
 from ..core import get_input_file, read_file
 
-BLANK = "."
+BLANK = -1
 
 
-def expand(data) -> str:
+def expand(data: str) -> list[int]:
     file_sizes = [int(ele) for ele in data.strip()[::2]]
     open_sizes = [int(ele) for ele in data.strip()[1::2]]
     expanded = []
     for file_id, (n_file, n_blank) in enumerate(zip_longest(file_sizes, open_sizes)):
         if n_file is not None:
-            expanded.extend([str(file_id)] * n_file)
+            expanded.extend([file_id] * n_file)
         if n_blank is not None:
             expanded.extend([BLANK] * n_blank)
-    return "".join(expanded)
+    return expanded
 
 
-def defrag(expanded):
+def defrag(expanded: list[int]) -> list[int]:
     defragged = list(expanded)
     max_n = len(defragged)
     for rev_id, ele in enumerate(reversed(expanded)):
@@ -32,27 +33,101 @@ def defrag(expanded):
             break
         defragged[idx] = ele
         defragged[max_id - 1] = BLANK
-    return "".join(defragged)
+    return defragged
 
 
-def checksum(defraged):
+def checksum(defraged, stop_on_blank=True):
     check = 0
     for idx, file_num in enumerate(defraged):
+        val = idx * int(file_num)
         if file_num == BLANK:
-            break
-        check += idx * int(file_num)
+            if stop_on_blank:
+                break
+            val = 0
+        check += val
     return check
 
 
 def main_part1(data):
     """Solution to day 09 part 1"""
     expanded = expand(data)
-    defraged = defrag(expanded)
-    return checksum(defraged)
+    defragged = defrag(expanded)
+    return checksum(defragged)
+
+
+@dataclass
+class FileBlock:
+    file_id: int
+    n: int
+
+
+# FileBlock = namedtuple("FileBlock", ["file_id", "n", "idx"])
+
+
+def convert_expanded(expanded) -> list[FileBlock]:
+    files = []
+    prev_id = expanded[0]
+    n = 0
+    start_idx = 0
+    for idx, file_id in enumerate(expanded):
+        if file_id == prev_id:
+            n += 1
+            continue
+        files.append(FileBlock(prev_id, n))
+        start_idx = idx
+        prev_id = file_id
+        n = 1
+    files.append(FileBlock(file_id, n))
+    return files
+
+
+def defrag_p2(files: list[FileBlock]) -> list[int]:
+    def _search(file):
+        mts = [
+            (idx, file) for idx, file in enumerate(defragged) if file.file_id == BLANK
+        ]
+        max_n = len(defragged)
+        i_file = max_n - i_rev - 1
+        for i_mt, mt in mts:
+            if i_mt >= i_file:
+                return
+            if mt.n < file.n:
+                continue
+            if mt.n == file.n:
+                defragged[i_file] = mt
+                defragged[i_mt] = file
+                return
+            remaining = FileBlock(BLANK, mt.n - file.n)
+            displaced = FileBlock(BLANK, file.n)
+            # update file and origin MT
+            defragged[i_file] = displaced
+            defragged[i_mt] = remaining
+            # insert file
+            defragged.insert(i_mt, file)
+            return
+
+    defragged = list(files)
+    for i_rev, file in enumerate(reversed(files)):
+        if file.file_id == BLANK:
+            continue
+        _search(file)
+    return defragged
+
+
+def convert_files(files: list[FileBlock]) -> list[int]:
+    expanded = []
+    for file in files:
+        expanded += [file.file_id] * file.n
+    return expanded
 
 
 def main_part2(data):
     """Solution to day 09 part 2"""
+    expanded = expand(data)
+    files = convert_expanded(expanded)
+    defragged = defrag_p2(files)
+    defragged_list = convert_files(defragged)
+    return checksum(defragged_list, False)
 
 
 @click.group
