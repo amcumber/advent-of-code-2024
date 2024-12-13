8f32a839f8fe2c406ea4d08adb6d5dc61d3c5bbe
Aaron McUmber <20608451+amcumber@users.noreply.github.com>
2024-12-12 21:28:29 -0700

    feat(day9): prototype day9

--- aoc_2024/day09/soln.py
+++ aoc_2024/day09/soln.py
@@ -1,28 +1,53 @@
 from collections import defaultdict, namedtuple
-from itertools import product
+from itertools import product, zip_longest
 from pathlib import Path
 
 import click
 
-from ..core import (
-    get_input_file,
-    read_file,
-)
+from ..core import get_input_file, read_file
+
+BLANK = "."
 
 
-def expand(data): ...
+def expand(data) -> str:
+    file_sizes = [int(ele) for ele in data.strip()[::2]]
+    open_sizes = [int(ele) for ele in data.strip()[1::2]]
+    expanded = []
+    for file_id, (n_file, n_blank) in enumerate(zip_longest(file_sizes, open_sizes)):
+        if n_file is not None:
+            expanded.extend([str(file_id)] * n_file)
+        if n_blank is not None:
+            expanded.extend([BLANK] * n_blank)
+    return "".join(expanded)
 
 
-def defrag(expanded): ...
+def defrag(expanded):
+    defragged = list(expanded)
+    max_n = len(defragged)
+    for rev_id, ele in enumerate(reversed(expanded)):
+        max_id = max_n - rev_id
+        if ele == BLANK:
+            continue
+        if (idx := defragged.index(BLANK)) >= max_id:
+            break
+        defragged[idx] = ele
+        defragged[max_id - 1] = BLANK
+    return "".join(defragged)
 
 
-def checksum(defraged): ...
+def checksum(defraged):
+    check = 0
+    for idx, file_num in enumerate(defraged):
+        if file_num == BLANK:
+            break
+        check += idx * int(file_num)
+    return check
 
 
 def main_part1(data):
     """Solution to day 09 part 1"""
     expanded = expand(data)
-    defraged = defraged(expanded)
+    defraged = defrag(expanded)
     return checksum(defraged)
 
 
@@ -38,12 +63,12 @@ def cli():
 @cli.command()
 @click.option("--file", default=get_input_file(__file__))
 def part1(file):
-    data = read_file(Path(file))
+    data = read_file(Path(file), as_str=True)
     print(main_part1(data))
 
 
 @cli.command()
 @click.option("--file", default=get_input_file(__file__))
 def part2(file):
-    data = read_file(Path(file))
+    data = read_file(Path(file), as_str=True)
     print(main_part2(data))
--- aoc_2024/day09/test_day09.py
+++ aoc_2024/day09/test_day09.py
@@ -10,6 +10,23 @@ def input_():
 
 
 @pytest.fixture
+def toy_input():
+    input_ = "12345\n"
+    expanded = "0..111....22222"
+    defragged = "022111222......"
+    checksum = 60
+    return input_, expanded, defragged, checksum
+
+
+@pytest.fixture
+def ex_raw_input_expanded_p1(input_):
+    expanded = "00...111...2...333.44.5555.6666.777.888899"
+    defragged = "0099811188827773336446555566.............."
+    checksum = 1928
+    return input_, expanded, defragged, checksum
+
+
+@pytest.fixture
 def ex_raw_input_p1(input_):
     expected = 1928
     return input_, expected
@@ -19,6 +36,42 @@ def ex_raw_input_p1(input_):
 def ex_raw_input_p2(input_):
     expected = 10000
     return input_, expected
+
+
+def test_expanded_toy(toy_input):
+    input_, expected, *_ = toy_input
+    result = soln.expand(input_)
+    assert result == expected
+
+
+def test_expanded_long(ex_raw_input_expanded_p1):
+    input_, expected, *_ = ex_raw_input_expanded_p1
+    result = soln.expand(input_)
+    assert result == expected
+
+
+def test_defrag_toy(toy_input):
+    _, input_, expected, _ = toy_input
+    result = soln.defrag(input_)
+    assert result == expected
+
+
+def test_defrag_long(ex_raw_input_expanded_p1):
+    _, input_, expected, _ = ex_raw_input_expanded_p1
+    result = soln.defrag(input_)
+    assert result == expected
+
+
+def test_checksum_toy(toy_input):
+    *_, input_, expected = toy_input
+    result = soln.checksum(input_)
+    assert result == expected
+
+
+def test_checksum_long(ex_raw_input_expanded_p1):
+    *_, input_, expected = ex_raw_input_expanded_p1
+    result = soln.checksum(input_)
+    assert result == expected
 
 
 def test_main_part1(ex_raw_input_p1):
