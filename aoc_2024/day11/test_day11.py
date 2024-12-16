5048402979ea778d285e7fabd34b92b912225190
Aaron <20608451+amcumber@users.noreply.github.com>
2024-12-15 21:05:01 -0800

    debugging day 11 p2

--- aoc_2024/day11/test_day11.py
+++ aoc_2024/day11/test_day11.py
@@ -17,249 +17,30 @@ def ex_input_p1():
     ]
 
 
-def to_list(stringy):
-    listy = []
-    for ele in stringy:
-        if ele != ".":
-            listy.append(int(ele))
-        else:
-            listy.append(soln.BLANK)
-
-    return listy
+def test_main_part1(ex_input_p1):
+    input_, *_, expected = ex_input_p1
+    expected = soln.to_list(expected)
+    result = soln.main_part1(input_, generations=6)
+    assert result == len(expected)
 
 
-@pytest.fixture
-def toy_input():
-    b = soln.BLANK
-    input_ = "12345\n"
-    expanded = to_list("0..111....22222")
-    defragged = to_list("022111222......")
-    checksum = 60
-    file_expanded = [
-        soln.FileBlock(*x)
-        for x in (
-            (0, 1),
-            (b, 2),
-            (1, 3),
-            (b, 4),
-            (2, 5),
-        )
-    ]
-    return input_, expanded, defragged, checksum, file_expanded
+@pytest.mark.parametrize(
+    ["gen"], ((1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,))
+)
+def test_gen0(gen):
+    e = [0]
+    for _ in range(gen):
+        e = soln.blink(e)
+    expected = e
+    result = soln.gen0(gen)
+    assert result == len(expected)
 
 
-@pytest.fixture
-def ex_input(input_):
-    b = soln.BLANK
-    expanded = to_list("00...111...2...333.44.5555.6666.777.888899")
-    defragged_p1 = to_list("0099811188827773336446555566..............")
-    checksum = 1928
-    file_expanded = [
-        soln.FileBlock(*x)
-        for x in (
-            (0, 2),
-            (b, 3),
-            (1, 3),
-            (b, 3),
-            (2, 1),
-            (b, 3),
-            (3, 3),
-            (b, 1),
-            (4, 2),
-            (b, 1),
-            (5, 4),
-            (b, 1),
-            (6, 4),
-            (b, 1),
-            (7, 3),
-            (b, 1),
-            (8, 4),
-            (9, 2),
-        )
-    ]
-    return input_, expanded, defragged_p1, checksum, file_expanded
-
-
-@pytest.fixture
-def ex_raw_input_p1(input_):
-    expected = 1928
-    return input_, expected
-
-
-@pytest.fixture
-def ex_raw_input_p2(input_):
-    expected = 2858
-    return input_, expected
-
-
-def test_expanded_toy(toy_input):
-    input_, expected, *_ = toy_input
-    result = soln.expand(input_)
-    assert result == expected
-
-
-def test_expanded_long(ex_input):
-    input_, expected, *_ = ex_input
-    result = soln.expand(input_)
-    assert result == expected
-
-
-def test_defrag_toy(toy_input):
-    _, input_, expected, *_ = toy_input
-    result = soln.defrag(input_)
-    assert result == expected
-
-
-def test_defrag_long(ex_input):
-    _, input_, expected, *_ = ex_input
-    result = soln.defrag(input_)
-    assert result == expected
-
-
-def test_checksum_toy(toy_input):
-    *_, input_, expected, _ = toy_input
-    result = soln.checksum(input_)
-    assert result == expected
-
-
-def test_checksum_long(ex_input):
-    *_, input_, expected, _ = ex_input
-    result = soln.checksum(input_)
-    assert result == expected
-
-
-def test_main_part1(ex_raw_input_p1):
-    input_, expected = ex_raw_input_p1
-    result = soln.main_part1(input_)
-    assert result == expected
-
-
-def test_convert_expanded_toy(toy_input):
-    _, expanded, *_, file_expanded = toy_input
-    results = soln.convert_expanded(expanded)
-    assert results == file_expanded
-
-
-def test_convert_expanded_long(ex_input):
-    _, expanded, *_, file_expanded = ex_input
-    results = soln.convert_expanded(expanded)
-    assert results == file_expanded
-
-
-@pytest.fixture
-def part2_toy():
-    return to_list("00...111...2...333.44.5555.6666.777.888899")
-
-
-@pytest.fixture
-def part2_files():
-    b = soln.BLANK
-    return [
-        soln.FileBlock(*x)
-        for x in [
-            (0, 2),
-            (b, 3),
-            (1, 3),
-            (b, 3),
-            (2, 1),
-            (b, 3),
-            (3, 3),
-            (b, 1),
-            (4, 2),
-            (b, 1),
-            (5, 4),
-            (b, 1),
-            (6, 4),
-            (b, 1),
-            (7, 3),
-            (b, 1),
-            (8, 4),
-            (9, 2),
-        ]
-    ]
-
-
-@pytest.fixture
-def part2_defragged():
-    return to_list("00992111777.44.333....5555.6666.....8888..")
-
-
-@pytest.fixture
-def part2_defrag_files():
-    b = soln.BLANK
-    return [
-        soln.FileBlock(*x)
-        for x in [
-            (0, 2),
-            (9, 2),
-            (2, 1),
-            (1, 3),
-            (7, 3),
-            (b, 1),
-            (4, 2),
-            (b, 1),
-            (3, 3),
-            (b, 4),
-            (5, 4),
-            (b, 1),
-            (6, 4),
-            (b, 5),
-            (8, 4),
-            (b, 2),
-        ]
-    ]
-
-
-@pytest.fixture
-def ex_files(part2_toy, part2_files, part2_defragged, part2_defrag_files):
-    return part2_toy, part2_files, part2_defragged, part2_defrag_files
-
-
-def test_convert_files(ex_files):
-    expected, files, defragged, def_files = ex_files
-    result = soln.convert_files(files)
-    assert result == expected
-
-    expected = defragged
-    result = soln.convert_files(def_files)
-    assert result == expected
-
-
-def test_main_part2_convert(ex_files, ex_raw_input_p2):
-    _, checksum = ex_raw_input_p2
-    expanded, files, defragged, def_files = ex_files
-    r_files = soln.convert_expanded(expanded)
-    assert files == r_files
-
-
-def test_main_part2_convert_defrag(ex_files, ex_raw_input_p2):
-    _, checksum = ex_raw_input_p2
-    expanded, files, defragged, def_files = ex_files
-    first_soln = soln.convert_files(def_files)
-    assert first_soln == defragged
-
-    r_files = soln.convert_expanded(expanded)
-
-    r_def_files = soln.defrag_p2(r_files)
-
-    r_defragged = soln.convert_files(r_def_files)
-    assert r_defragged == defragged
-
-
-def test_main_part2_pipeline(ex_files, ex_raw_input_p2):
-    _, checksum = ex_raw_input_p2
-    expanded, files, defragged, def_files = ex_files
-    r_files = soln.convert_expanded(expanded)
-
-    r_def_files = soln.defrag_p2(r_files)
-
-    r_defragged = soln.convert_files(r_def_files)
-
-    r_check = soln.checksum(r_defragged, False)
-    assert r_check == checksum
-
-
-def test_main_part2(ex_raw_input_p2):
-    input_, expected = ex_raw_input_p2
-    result = soln.main_part2(input_)
-    assert result == expected
+@pytest.mark.parametrize(
+    ["gen", "expected"], [(1, 3), (2, 4), (3, 5), (4, 9), (5, 13), (6, 22)]
+)
+def test_main_part2(ex_input_p1, gen, expected):
+    input_, *_ = ex_input_p1
+    expected = soln.to_list(expected)
+    result = soln.main_part2(input_, generations=gen)
+    assert result == len(expected)
